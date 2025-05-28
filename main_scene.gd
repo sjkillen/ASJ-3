4d8add507f9719135113d9f2c25a9f634fc3c307
jamie-hello <jamiecrom22@gmail.com>
2025-05-28 07:10:31 -0400

    jamie's vision, very rough, very incomplete

--- main_scene.gd
+++ main_scene.gd
@@ -3,9 +3,11 @@ extends Node3D
 func _process(_delta):
 	pass
 	
-func _input(_event):
-	#if event.is_action_pressed("debug button"):
-	#	Globals.level += 1
+func _input(event):
+	if event.is_action_pressed("debug button"):
+		$"big empty forest/SpotLight3D".visible = true
+		$"big empty forest/SpotLight3D2".visible = true
+		$"big empty forest/SpotLight3D3".visible = true
 	pass
 
 func _on_level_2_body_entered(body):
