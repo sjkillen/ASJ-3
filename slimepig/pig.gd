daef2b312a729ae59f77e2a1dee808f53f77cf98
Spencer Killen <sjkillen@ualberta.ca>
2025-06-02 23:32:54 -0600

    Add idle animation

--- slimepig/pig.gd
+++ slimepig/pig.gd
@@ -2,5 +2,8 @@ extends Node3D
 
 @onready var animation: AnimationNodeStateMachinePlayback = $AnimationTree.get("parameters/playback")
 
-func _ready() -> void:
-	animation.travel("Test")
+func walk() -> void:
+	animation.travel("Walking")
+	
+func idle() -> void:
+	animation.travel("Idle")
